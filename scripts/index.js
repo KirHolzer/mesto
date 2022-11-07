4f9ac748bba7173b5006931b6935647251d4ced1
Kirill Goltser <kirill.holzer@yandex.ru>
2022-11-07 12:18:03 +0300

    DONE: popup and JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,37 @@
-const editButton = document.querySelector(".profile__edit-button");
-const popup = document.querySelector(".popup");
-const popupClose = popup.querySelector('.popup__close-button');
+const editButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const CloseButton = popup.querySelector('.popup__close-button');
+let profileName = document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__profession');
+
+
 
 editButton.addEventListener('click', () => (
   popup.classList.add('popup_opened')
 ))
 
-popupClose.addEventListener('click', () => (
+
+CloseButton.addEventListener('click', () => (
   popup.classList.remove('popup_opened')
 ))
+
+function popupClose () {
+  popup.classList.remove('popup_opened');
+}
+
+let formElement = document.querySelector('.popup__form-container');
+let nameInput = formElement.querySelector('.popup__input-name');
+let jobInput = formElement.querySelector('.popup__input-job');
+
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  let name = nameInput.value;
+  let profession = jobInput.value;
+  profileName.textContent = name;
+  profileProfession.textContent = profession;
+  popupClose();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
