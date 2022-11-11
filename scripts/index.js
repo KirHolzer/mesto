73f01d0ec4b240f46c9e31a9ed67a4bc3e89e293
Kirill Goltser <kirill.holzer@yandex.ru>
2022-11-12 00:19:00 +0300

    some fixes from review

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const closeButton = editPopup.querySelector('.popup__close-button');
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
 const formElement = document.querySelector('.popup__form-container');
-const nameInput = formElement.querySelector('.popup__input_name');
-const jobInput = formElement.querySelector('.popup__input_job');
+const nameInput = formElement.querySelector('.popup__input_type_name');
+const jobInput = formElement.querySelector('.popup__input_type_job');
 
 const popupOpen = function () {
   editPopup.classList.add('popup_opened');
