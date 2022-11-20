8fe6e1f2bd2528412cb1e4390de71252c3a0b0d2
Kirill Goltser <kirill.holzer@yandex.ru>
2022-11-20 21:46:17 +0300

    ADDED: new popup in HTML

--- scripts/index.js
+++ scripts/index.js
@@ -8,12 +8,6 @@ const nameInput = formElement.querySelector('.popup__input_type_name');
 const jobInput = formElement.querySelector('.popup__input_type_job');
 
 
-// const newElement = elementsTemplate.querySelector(".elements__item").cloneNode(true);
-function addElement(item) {
-  newElement.querySelector(".elements__text").textContent = item.name;
-  newElement.querySelector(".elements__image").src = item.link;
-  return newElement;
-}
 
 
 
@@ -25,25 +19,6 @@ function addElement(item) {
 
 
 
-
-const popupOpen = function () {
-  editPopup.classList.add('popup_opened');
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileProfession.textContent;
-}
-
-const popupClose = function() {
-  editPopup.classList.remove('popup_opened');
-}
-
-function formSubmitHandler (evt) {
-  evt.preventDefault();
-  const name = nameInput.value;
-  const profession = jobInput.value;
-  profileName.textContent = name;
-  profileProfession.textContent = profession;
-  popupClose();
-}
 
 formElement.addEventListener('submit', formSubmitHandler);
 editButton.addEventListener('click', popupOpen);
