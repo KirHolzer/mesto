aa256a28168f0f13bdccfcc9ccff088307218053
Kirill Goltser <kirill.holzer@yandex.ru>
2022-11-20 21:06:30 +0300

    ADDED: tamplate

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,25 @@ const formElement = document.querySelector('.popup__form-container');
 const nameInput = formElement.querySelector('.popup__input_type_name');
 const jobInput = formElement.querySelector('.popup__input_type_job');
 
+
+// const newElement = elementsTemplate.querySelector(".elements__item").cloneNode(true);
+function addElement(item) {
+  newElement.querySelector(".elements__text").textContent = item.name;
+  newElement.querySelector(".elements__image").src = item.link;
+  return newElement;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 const popupOpen = function () {
   editPopup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
